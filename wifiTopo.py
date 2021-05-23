0cc943a49ab2e0bd08e2a851f5a86721a0273c71
NickShen4Github <59556340+NickShen4Github@users.noreply.github.com>
2021-05-23 20:34:15 +0800

    Update wifiTopo.py

--- wifiTopo.py
+++ wifiTopo.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-from mininet.node import Controller, OVSKernelSwitch
+from mininet.node import Controller, OVSKernelSwitch, Host
 from mininet.log import setLogLevel, info
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.node import Station, OVSKernelAP
 from mn_wifi.cli import CLI
-from mn_wifi.link import wmediumd#, mesh
+from mn_wifi.link import wmediumd
 from mn_wifi.wmediumdConnector import interference
 from subprocess import call
 
@@ -26,21 +26,27 @@ def myNetwork():
 
     info( '*** Add switches/APs\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-
-    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, ssid='ap1-ssid',
-                             channel='1', mode='g', passwd = 'SDN123456', encrypt = 'wpa2', position='350.0,150.0,0')
-    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, ssid='ap2-ssid',
-                             channel='1', mode='g', passwd = 'SDN123456', encrypt = 'wpa2', position='350.0,250.0,0')
     ap3 = net.addAccessPoint('ap3', cls=OVSKernelAP, ssid='ap3-ssid',
-                             channel='1', mode='g', passwd = 'SDN123456', encrypt = 'wpa2', position='350.0,350.0,0')
+                             channel='1', mode='g', position='348.0,73.0,0')
+    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, ssid='ap1-ssid',
+                             channel='1', mode='g', position='349.0,361.0,0')
     ap4 = net.addAccessPoint('ap4', cls=OVSKernelAP, ssid='ap4-ssid',
-                             channel='1', mode='g', passwd = 'SDN123456', encrypt = 'wpa2', position='550.0,250.0,0')
+                             channel='1', mode='g', position='572.0,352.0,0')
+    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, ssid='ap2-ssid',
+                             channel='1', mode='g', position='346.0,197.0,0')
 
     info( '*** Add hosts/stations\n')
-    sta2 = net.addStation('sta2', passwd = 'SDN123456', encrypt = 'wpa2', ip='10.0.0.2',
-                           position='350.0,50.0,0')
-    sta1 = net.addStation('sta1', passwd = 'SDN123456', encrypt = 'wpa2', ip='10.0.0.1',
-                           position='320.0,60.0,0')
+    sta2 = net.addStation('sta2', ip='10.0.0.2',
+                           position='386.0,459.0,0')
+    sta4 = net.addStation('sta4', ip='10.0.0.4',
+                           position='433.0,190.0,0')
+    sta5 = net.addStation('sta5', ip='10.0.0.5',
+                           position='435.0,76.0,0')
+    sta3 = net.addStation('sta3', ip='10.0.0.3',
+                           position='590.0,432.0,0')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    sta1 = net.addStation('sta1', ip='10.0.0.1',
+                           position='305.0,455.0,0')
 
     info("*** Configuring Propagation Model\n")
     net.setPropagationModel(model="logDistance", exp=3)
@@ -49,16 +55,16 @@ def myNetwork():
     net.configureWifiNodes()
 
     info( '*** Add links\n')
-    net.addLink(sta1, ap1)
-    net.addLink(sta2, ap1)
-    #net.addLink(ap1, intf='ap1-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
-    #net.addLink(ap2, intf='ap2-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
-    #net.addLink(ap3, intf='ap3-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
-    #net.addLink(ap4, intf='ap4-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
-    net.addLink(ap1, ap2)
-    net.addLink(ap2, ap3)
+    net.addLink(ap3, ap2)
+    net.addLink(ap2, ap1)
     net.addLink(ap1, ap4)
     net.addLink(s1, ap1)
+    net.addLink(h1, s1)
+    net.addLink(ap1, sta1)
+    net.addLink(ap1, sta2)
+    net.addLink(ap4, sta3)
+    net.addLink(sta4, ap2)
+    net.addLink(sta5, ap3)
 
     net.plotGraph(max_x=1000, max_y=1000)
 
@@ -71,9 +77,9 @@ def myNetwork():
     info( '*** Starting switches/APs\n')
     net.get('s1').start([c0])
     net.get('ap3').start([c0])
-    net.get('ap2').start([c0])
     net.get('ap1').start([c0])
     net.get('ap4').start([c0])
+    net.get('ap2').start([c0])
 
     info( '*** Post configure nodes\n')
 
@@ -84,4 +90,3 @@ def myNetwork():
 if __name__ == '__main__':
     setLogLevel( 'info' )
     myNetwork()
-
