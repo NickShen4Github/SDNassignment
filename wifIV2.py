285793b0fda62ee69d66a0e3327f8ce48313a275
NickShen4Github <59556340+NickShen4Github@users.noreply.github.com>
2021-05-24 09:34:22 +0800

    Add files via upload

---
+++ wifIV2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+from mininet.node import Controller, OVSKernelSwitch, Host
+from mininet.log import setLogLevel, info
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.node import Station, OVSKernelAP
+from mn_wifi.cli import CLI
+from mn_wifi.link import wmediumd
+from mn_wifi.wmediumdConnector import interference
+from subprocess import call
+
+
+def myNetwork():
+
+    net = Mininet_wifi(topo=None,
+                       build=False,
+                       link=wmediumd,
+                       wmediumd_mode=interference,
+                       ipBase='172.16.0.0/16')
+
+    info( '*** Adding controller\n' )
+    c0 = net.addController(name='c0',
+                           controller=Controller,
+                           protocol='tcp',
+                           port=6633)
+
+    info( '*** Add switches/APs\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    ap3 = net.addAccessPoint('ap3', cls=OVSKernelAP, ssid='ap3-ssid',
+                             channel='1', mode='g', position='348.0,73.0,0')
+    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, ssid='ap1-ssid',
+                             channel='1', mode='g', position='349.0,361.0,0')
+    ap4 = net.addAccessPoint('ap4', cls=OVSKernelAP, ssid='ap4-ssid',
+                             channel='1', mode='g', position='572.0,352.0,0')
+    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, ssid='ap2-ssid',
+                             channel='1', mode='g', position='346.0,197.0,0')
+
+    info( '*** Add hosts/stations\n')
+    sta2 = net.addStation('sta2', ip='172.16.2.202',
+                           position='386.0,459.0,0')
+    sta4 = net.addStation('sta4', ip='172.16.2.204',
+                           position='433.0,190.0,0')
+    sta5 = net.addStation('sta5', ip='172.16.2.205',
+                           position='435.0,76.0,0')
+    sta3 = net.addStation('sta3', ip='172.16.4.203',
+                           position='590.0,432.0,0')
+    h1 = net.addHost('h1', cls=Host, ip='172.16.0.1', defaultRoute=None)
+    sta1 = net.addStation('sta1', ip='172.16.2.201',
+                           position='305.0,455.0,0')
+
+    info("*** Configuring Propagation Model\n")
+    net.setPropagationModel(model="logDistance", exp=3)
+
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+
+    info( '*** Add links\n')
+    net.addLink(ap3, ap2)
+    net.addLink(ap2, ap1)
+    net.addLink(ap1, ap4)
+    net.addLink(s1, ap1)
+    net.addLink(h1, s1)
+    net.addLink(ap1, sta1)
+    net.addLink(ap1, sta2)
+    net.addLink(ap4, sta3)
+    net.addLink(sta4, ap2)
+    net.addLink(sta5, ap3)
+
+    net.plotGraph(max_x=1000, max_y=1000)
+
+    info( '*** Starting network\n')
+    net.build()
+    net.addNAT(name = 'nat1',LinkTo = 'ap1',ip = '172.16.2.61').configDefault()
+    net.addNAT(name = 'nat2',LinkTo = 'ap2',ip = '172.16.2.62').configDefault()
+    net.addNAT(name = 'nat3',LinkTo = 'ap3',ip = '172.16.2.63').configDefault()
+    net.addNAT(name = 'nat4',LinkTo = 'ap4',ip = '172.16.4.41').configDefault()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches/APs\n')
+    net.get('s1').start([c0])
+    net.get('ap3').start([c0])
+    net.get('ap1').start([c0])
+    net.get('ap4').start([c0])
+    net.get('ap2').start([c0])
+
+    info( '*** Post configure nodes\n')
+
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
